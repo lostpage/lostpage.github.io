dcfc9ac0ad5505a100e49e2e1f498b0394ebec5b
lostpage <101554362+lostpage@users.noreply.github.com>
2024-10-31 19:58:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
         // Инициализация сцены, камеры и рендерера
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, 720 / 1080, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer();
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(720, 1080);
         document.body.appendChild(renderer.domElement);
 
         // Создание платформы
